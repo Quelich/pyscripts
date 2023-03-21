06c8c0c8f70a2675425642a1e197d930f23418d0
Emre Kılıç <emre1kilic@outlook.com>
2023-03-22 00:32:01 +0300

    add directives

---
+++ resize_marbles.py
@@ -0,0 +1,18 @@
+
+
+
+
+# todo
+# 1. step
+# get source directory as argument
+# count each file in directory
+# print file names 
+# count files 
+
+# 2. step
+# make a new directory of pixel size, e.g., 100x100
+# change the size of images (.png, .jpg, .jpeg) to, e.g., 100x100(customizable)
+
+
+
+print("Hello world")
